e6697a4dd3e3e8a5862aab54c4977812cbb84ba3
toast <88937360+fullytoast@users.noreply.github.com>
2022-05-03 02:26:00 +0000

    add environment variables to dcrd docker-compose and use veriables in start-dcrd

--- apps/decred-dcrd/build/start-dcrd.sh
+++ apps/decred-dcrd/build/start-dcrd.sh
@@ -3,12 +3,15 @@ set -e
 
 #checks to see if cert/key exist, creates if missinn
 if [[ ! -f /storage/certs/dcrd.cert || ! -f /storage/certs/dcrd.key ]]; then
-  gencerts -H=10.21.21.95 -f /storage/certs/dcrd.cert /storage/certs/dcrd.key
-  echo "## Generating RPC certificate and key ###"
+  gencerts -H=$APP_DECRED_DCRD_IP -f /storage/certs/dcrd.cert /storage/certs/dcrd.key
+  echo "## RPC certificate and key not found, generating... ##\n"
 fi
 
+echo "## dcrd tor hidden service onion address ##"
+echo "   $APP_HIDDEN_SERVICE\n"
+
 # Set dcrd options
-echo "## Start dcrd ##"
+echo "## Starting dcrd... ##\n"
 dcrd_options=(
   --datadir=/storage/dcrd
   --logdir=/storage/logs
@@ -16,9 +19,10 @@ dcrd_options=(
   --rpckey=/storage/certs/dcrd.key
   --rpcuser=testuser
   --rpcpass=testpass
-  --rpclisten=:9109
+  --rpclisten=:$APP_DECRED_DCRD_RPC_PORT
   --txindex
   --addrindex
 )
+# add tor config options to dcrd
 
 dcrd "${dcrd_options[@]}"
